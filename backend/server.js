27eb005b7b9037774a045a2a3f09c202f74b3cb2
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-01-09 18:36:05 +0200

    Build the categories schema

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,8 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const db = require("./database/db");
-
 app.use(cors());
-
 app.use(express.json());
-
 const PORT = 5000;
 
 app.listen(PORT, () => {
