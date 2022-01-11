0645fdbf7e1725c399df1c62e5c836b016f1403a
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-01-11 13:15:48 +0200

    getProductByName

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -7,20 +7,37 @@ const {
   updateProductById,
   deleteProductById,
   getProductsByCategory,
+  getProductByName,
 } = require("../controllers/products");
 
-/*************************************************** */
+/****************************************************************** */
 const authentication = require("../middleware/authentication");
-const { authorization } = require("../middleware/authorization");
-/************************************************ */
+const  authorization  = require("../middleware/authorization");
+/****************************************************************** */
 
 const productsRouter = express.Router();
 
-productsRouter.post("/", createNewProduct);
+productsRouter.post(
+  "/",
+  authentication,
+  authorization("Add_Products"),
+  createNewProduct
+);
 productsRouter.get("/", getAllProducts);
 productsRouter.get("/:id", getProductById);
-productsRouter.put("/:id", updateProductById);
-productsRouter.delete("/:id", deleteProductById);
+productsRouter.put(
+  "/:id",
+  authentication,
+  authorization("Add_Products"),
+  updateProductById
+);
+productsRouter.delete(
+  "/:id",
+  authentication,
+  authorization("Add_Products"),
+  deleteProductById
+);
 productsRouter.get("/categories/:id", getProductsByCategory);
+productsRouter.get("/:name", getProductByName);
 
 module.exports = productsRouter;
