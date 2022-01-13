ed258b53f69f78bdb38b1ecc776702c95c0d7a6a
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-01-13 22:47:15 +0200

    Create new Route to add products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
 import "./App.css";
-import React from "react";
+import React, { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import ModifyProduct from "./components/ModifyProduct";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/products/admin" element={<ModifyProduct />} />
       </Routes>
     </div>
   );
