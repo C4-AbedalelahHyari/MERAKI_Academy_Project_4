1941b50d48b53d38ba5b4d2204f965d1210488ca
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-01-13 22:50:05 +0200

    Create new component called Home to getAllProducts in it

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,21 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Product from "./Product";
+import banner from "../images/banner.png";
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const getAllProducts = async () => {
+    try {
+      const result = await axios.get("http://localhost:5000/products/");
+      if (result.data.success) {
+        setProducts(result.data.products);
+      } else {
+        console.log(`cannot Get`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllProducts();
+  }, []);
   return (
     <>
       <div className="home">
-        <nav className="navbar">
-          <Link className="Link" to="/">
-            Home
-          </Link>
-          <Link className="Link" to="/register">
-            Register
-          </Link>
-          <Link className="Link" to="/login">
-            Login
-          </Link>
-        </nav>
+        <div className="banner">
+          <img className="image-banner" src={banner} />
+        </div>
+        <div className="products">
+          {products.map((element, index) => {
+            return (
+              <Product
+                imageSrc={element.imageSrc}
+                name={element.name}
+                price={element.price}
+                rating={element.rating}
+                views={element.views}
+              />
+            );
+          })}
+        </div>
       </div>
     </>
   );
