37c88cf63205e8f3fcd0f6cbc7a2879703e51703
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-01-14 23:41:37 +0200

    Add Banner to the home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,43 +1,18 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import Product from "./Product";
-import banner from "../images/banner.png";
+import Product from "./Products";
+import banner from "../images/banner.jpg";
+
 const Home = () => {
-  const [products, setProducts] = useState([]);
-  const getAllProducts = async () => {
-    try {
-      const result = await axios.get("http://localhost:5000/products/");
-      if (result.data.success) {
-        setProducts(result.data.products);
-      } else {
-        console.log(`cannot Get`);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    getAllProducts();
-  }, []);
+  
   return (
     <>
       <div className="home">
         <div className="banner">
           <img className="image-banner" src={banner} />
         </div>
-        <div className="products">
-          {products.map((element, index) => {
-            return (
-              <Product
-                imageSrc={element.imageSrc}
-                name={element.name}
-                price={element.price}
-                rating={element.rating}
-                views={element.views}
-              />
-            );
-          })}
-        </div>
+        <h1 className="text">Products</h1>
+        <Product />
       </div>
     </>
   );
