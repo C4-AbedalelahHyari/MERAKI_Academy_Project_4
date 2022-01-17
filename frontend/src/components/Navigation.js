a43db745f66945169bbdf30a4fb9ab290b934cbe
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-01-17 12:08:18 +0200

    Add new Link to the navBar Compnent called My Orders

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,41 +1,38 @@
 import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../context/user";
-import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Search, Security, ShoppingCartOutlined } from "@material-ui/icons";
 import { Badge, Input } from "@material-ui/core";
 import axios from "axios";
+/******************************************************************* */
 const Navigation = () => {
   const { logout, token } = useContext(UserContext);
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
   const [name, setName] = useState("");
-  /******************************************** */
+  /*************************************************** */
   const getAllProducts = async () => {
     try {
       const result = await axios.get("http://localhost:5000/products/");
       if (result.data.success) {
         setProducts(result.data.products);
       } else {
-        console.log(`cannot Get`);
+        console.log(`cannot Get the Products`);
       }
     } catch (error) {
       console.log(error);
     }
   };
+  getAllProducts();
   const func = () => {
-    const search1 = products
+    const result = products
       ? products.find((element, index) => {
-          if (element.name == name) {
+          if (element.name.toLowerCase() == name.toLowerCase()) {
             navigate(`/product/${element._id}`);
           }
         })
       : "Nothing To Show";
   };
-  getAllProducts();
-  // useEffect(() => {
-  //   getAllProducts();
-  //   func();
-  // }, [products]);
 
   return (
     <div className="nav-container">
@@ -45,6 +42,9 @@ const Navigation = () => {
             <div className="left">
               <Link className="nav-items" to="/">
                 Home
+              </Link>
+              <Link className="nav-items" to="/myOrders">
+                My Orders
               </Link>
             </div>
             <div className="center">
@@ -66,17 +66,39 @@ const Navigation = () => {
               </div>
             </div>
             <div className="right">
+              <div className="together">
+                <Security
+                  className="nav-items"
+                  style={{ color: "white", cursor: "pointer", fontSize: 35 }}
+                />
+                <Link className="nav-items" to="/admin">
+                  Admin Panel
+                </Link>
+              </div>
               <button className="log-out" onClick={logout}>
                 LogOut
               </button>
+              <div className="together">
+                <Badge badgeContent={6} color="secondary" />
+                <Link className="nav-items" to="/cart">
+                  <ShoppingCartOutlined
+                    style={{ fontSize: 35, cursor: "pointer" }}
+                  />
+                </Link>
+              </div>
             </div>
           </>
         ) : (
           <>
             <div className="left">
-              <Link className="nav-items" to="/">
-                Home
-              </Link>
+              <div className="together_2">
+                <Link className="nav-items" to="/">
+                  Home
+                </Link>
+                <Link className="nav-items" to="/myOrders">
+                  My Orders
+                </Link>
+              </div>
             </div>
 
             <div className="center">
